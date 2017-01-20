b45a622318f1c6e575646f1098c496a0ec0c776d
Nick McKinney <nick@kmonkey.net>
2017-01-19 19:17:03 -0600

    Add constants to control unit for readability

--- control_unit.v
+++ control_unit.v
@@ -12,10 +12,15 @@ module control_unit (
 	output [9:0] dbg_state
 );
 
+	localparam FETCH = 10'h1;
+	localparam DECODE = 10'h2;
+	localparam ALU = 10'h4;
+	localparam MEM = 10'h8;
+	localparam REG_WRITE = 10'h10;
 	reg [9:0] current_state;
 	
 	initial begin
-		current_state = 10'b1;
+		current_state = FETCH;
 	end
 	
 	assign fetch_en = current_state[0];
@@ -27,9 +32,9 @@ module control_unit (
 	always @(posedge clk) begin
 		if(!needWait) begin
 			case(current_state)
-				10'b1: current_state <= 10'b10;
-				10'b10: current_state <= 10'b100;
-				10'b100: current_state <= 10'b1;
+				FETCH: current_state <= DECODE;
+				DECODE: current_state <= ALU;
+				ALU: current_state <= FETCH;
 			endcase
 		end
 	end
