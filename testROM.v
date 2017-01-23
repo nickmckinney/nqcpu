d6391d49a36cca51b8efad095ce534f62e4ff9fb
Nick McKinney <nick@kmonkey.net>
2017-01-22 19:57:50 -0600

    Add memory read operations to test program

--- testROM.v
+++ testROM.v
@@ -27,15 +27,22 @@ module testROM (
 				15'h1: romData <= 16'h0102;
 				15'h2: romData <= 16'h0326;
 				15'h3: romData <= 16'h054a;
-				15'h4: romData <= 16'h5c0a;
-				15'h5: romData <= 16'h5d00;
-				15'h6: romData <= 16'h44c2;
-				15'h7: romData <= 16'h5201;
-				15'h8: romData <= 16'h0aa0;
-				15'h9: romData <= 16'h0004;
-				15'ha: romData <= 16'h0809;
-				15'hb: romData <= 16'h6afa;
-				15'hc: romData <= 16'h6e00;
+				15'h4: romData <= 16'h0dda;
+				15'h5: romData <= 16'h5d01;
+				15'h6: romData <= 16'h4dc3;
+				15'h7: romData <= 16'h42c2;
+				15'h8: romData <= 16'h5300;
+				15'h9: romData <= 16'h5a08;
+				15'ha: romData <= 16'h15d4;
+				15'hb: romData <= 16'h0bb6;
+				15'hc: romData <= 16'h0aa0;
+				15'hd: romData <= 16'h0004;
+				15'he: romData <= 16'h0809;
+				15'hf: romData <= 16'h6afa;
+				15'h10: romData <= 16'h6e00;
+
+				15'h80: romData <= 16'h1001;
+
 				default: romData <= 16'hf000;
 			endcase
 		end
