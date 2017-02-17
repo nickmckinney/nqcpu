c545a6fb3e5e569f0da1bf496b78994b8d16f090
Nick McKinney <nick@kmonkey.net>
2017-02-16 20:24:44 -0600

    Add memory address comments so debug state is easier to follow

--- testROM.v
+++ testROM.v
@@ -23,26 +23,26 @@ module testROM (
 			read_finished <= 1'b1;
 
 			case(addr_i[15:1])
-				15'h00: romData <= 16'h0bb6;
-				15'h01: romData <= 16'h0102;
-				15'h02: romData <= 16'h0326;
-				15'h03: romData <= 16'h054a;
-				15'h04: romData <= 16'h0dda;
-				15'h05: romData <= 16'h5d01;
-				15'h06: romData <= 16'h4dc3;
-				15'h07: romData <= 16'h42c2;
-				15'h08: romData <= 16'h5300;
-				15'h09: romData <= 16'h5a08;
-				15'h0a: romData <= 16'h15d4;
-				15'h0b: romData <= 16'h0bb6;
-				15'h0c: romData <= 16'h0aa0;
-				15'h0d: romData <= 16'h0004;
-				15'h0e: romData <= 16'h0809;
-				15'h0f: romData <= 16'h6afa;
-				15'h10: romData <= 16'h9148;
-				15'h11: romData <= 16'h0dda;
-				15'h12: romData <= 16'h41ba;
-				15'h13: romData <= 16'h6e00;
+				15'h00: romData <= 16'h0bb6;  // 00
+				15'h01: romData <= 16'h0102;  // 02
+				15'h02: romData <= 16'h0326;  // 04
+				15'h03: romData <= 16'h054a;  // 06
+				15'h04: romData <= 16'h0dda;  // 08
+				15'h05: romData <= 16'h5d01;  // 0A
+				15'h06: romData <= 16'h4dc3;  // 0C
+				15'h07: romData <= 16'h42c2;  // 0E
+				15'h08: romData <= 16'h5300;  // 10
+				15'h09: romData <= 16'h5a08;  // 12
+				15'h0a: romData <= 16'h15d4;  // 14
+				15'h0b: romData <= 16'h0bb6;  // 16
+				15'h0c: romData <= 16'h0aa0;  // 18
+				15'h0d: romData <= 16'h0004;  // 1A
+				15'h0e: romData <= 16'h0809;  // 1C
+				15'h0f: romData <= 16'h6afa;  // 1E
+				15'h10: romData <= 16'h9148;  // 20
+				15'h11: romData <= 16'h0dda;  // 22
+				15'h12: romData <= 16'h41ba;  // 24
+				15'h13: romData <= 16'h6e00;  // 26
 				15'h14: romData <= 16'hf000;
 
 				15'h80: romData <= 16'h1001;
